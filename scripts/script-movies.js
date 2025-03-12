d1f23f3561b44f3d51ece00bcc483ddeb5cd02f6
DeyvidAugusto <deyvidaugusto100@gmail.com>
2025-03-12 14:18:46 -0300

    Add pagination

--- scripts/script-movies.js
+++ scripts/script-movies.js
@@ -5,11 +5,45 @@ const IMG_URL = "https://image.tmdb.org/t/p/w500";
 const list = document.querySelector('div.search-movie-list');
 const SEARCH_URL = `${BASE_URL}/search/movie?api_key=${API_KEY}&query="`;
 
+/* Paginação */
+
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const current = document.getElementById('current')
+
+var currentPage = 1;
+var nextPage = 2;
+var prevPage = 3;
+var lastUrl = '';
+var totalPages = 100;
+
+/* Paginação */
+
 getMovies(API_URL)
 function getMovies(url) {
+  lastUrl = url /* <- coisa de paginação*/
     fetch(url).then(res => res.json()). then(data => {
         if(data.results.length !== 0) {
           showMovies(data.results);
+          currentPage = data.page; /* <- coisa de paginação*/
+          nextPage = currentPage + 1; /* <- coisa de paginação*/
+          prevPage = currentPage - 1; /* <- coisa de paginação*/
+          totalPages = data.total_pages; /* <- coisa de paginação*/
+
+          current.innerHTML = currentPage;/* <- coisa de paginação*/
+
+          if(currentPage <= 1){ /* <- coisa de paginação*/
+            prev.classList.add('disable');
+            next;classList.remove('disable')
+          }
+          else if(currentPage >= totalPages){
+            prev.classList.remove('disable');
+            next;classList.add('disable')
+          }
+          else{
+            prev.classList.remove('disable');
+            next;classList.remove('disable')
+          }
         }
         else {
           list.innerHTML = "<h1 class='no-results'>No Results Found</h1>";
@@ -181,3 +215,35 @@ frmPesquisa.onsubmit = (ev) => {
     localStorage.setItem('pesquisa', pesquisa);
     window.location.href = 'results.html';
 };
+
+
+/* Paginação */
+prev.addEventListener('click', () => {
+  if(prevPage > 0){
+    pageCall(prevPage);
+  }
+})
+
+next.addEventListener('click', () => {
+  if(nextPage <= totalPages){
+    pageCall(nextPage);
+  }
+})
+
+function pageCall(page){
+  let urlSplit = lastUrl.split('?');
+  let queryParams = urlSplit[1].split('&');
+  let key = queryParams[queryParams.length -1].split('=');
+  if(key[0] != 'page'){
+    let url = lastUrl + '&page=' + page
+    getMovies(url)
+  }
+  else {
+    key[1] = page.toString();
+    let a = key.join('=');
+    queryParams[queryParams.length -1] = a;
+    let b = queryParams.join('&');
+    let url = urlSplit[0] +'?'+ b;
+    getMovies(url)
+  }
+}
